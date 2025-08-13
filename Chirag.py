772a13411ca3a0ef4b4366fccd445123fa6c812f
Aaditya Singh Naruka <singhadi2306@gmail.com>
2025-08-13 15:31:09 +0530

    Aadi-pc commit

---
+++ Chirag.py
@@ -0,0 +1,17 @@
+a=4
+b=7
+print(a+b)
+c=9
+d=10
+print(c*d)
+e=11
+f=12
+print(f-e)
+g=15
+h=45
+print(h/g)
+print("This is a test file in Master branch")
+print("This is a test file in Slave branch")
+print("Mai third user hu")
+print("edited by Aaditya Singh Naruka")
+print("Aadi system edit")
